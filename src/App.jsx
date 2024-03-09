2e35163799884bd88ac4e323ba99f972a54b6604
fanrendejuzi <90998758+fanrendejuzi@users.noreply.github.com>
2024-03-09 15:27:48 -0500

    Hero

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import './App.css'; // Import your CSS file
+import Home from './components/pages/Home'
+
+function App () {
+  return ( 
+    <Router>
+      <Navbar /> 
+      <Routes>
+        <Route path='/' exact component=
+        {Home} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
